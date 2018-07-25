c024f7048f648a3fb15db4f47d7a6b3ce1897414
Ivan <ivanmartini96@gmail.com>
2018-07-25 10:16:22 +0200

    first work

--- index.js
+++ index.js
@@ -3,7 +3,43 @@ const express = require('express')
 
 var app = express()
 
+//{code:'',departureAirport:'',arrivalAirport:'',delay:}
+var flights = [
+			{code:'SQ388',departureAirport:'MXP',arrivalAirport:'JFK',delay:37},
+			{code:'UA928',departureAirport:'JAL',arrivalAirport:'EGS',delay:53}
+			{code:'GV473',departureAirport:'RBJ',arrivalAirport:'VRE',delay:21}
+			]
+
+
+//homepage routing
 app.get('/', function(req, res){
+  res.writeHead(200);
+  res.write('It works!')
+  res.write('require with /flights/airportCode/selector all the flights')
+  res.write('require with /flightInfo/flightCode all the info for that flight')
+  res.write('post request with /flights to insert a flight')
+  res.end();
+});
+
+//flight list routing
+app.get('/flights', function(req, res){
+  console.log("request on " + request.url " - sending all flights info")
+  console.log(request.url)
+
+  res.writeHead(200);
+  res.write('It works! flights')
+  res.end();
+});
+
+//flight info routing
+app.get('/flightInfo', function(req, res){
+  res.writeHead(200);
+  res.write('It works! flight Info')
+  res.end();
+});
+
+//insert a flight
+app.post('/flights', function(req, res){
   res.writeHead(200);
   res.write('It works! Express')
   res.end();
