87fe943b6af81f17b2bfa0855f26de17e940c136
Ivan <ivanmartini96@gmail.com>
2018-07-25 09:55:50 +0200

    1st bug fix

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const http = require('http')
 const bodyParser = require('body-parser')
 
 http.createServer(function(req,res) {
-	var URL = req.url
-
+	console.log("Got a request, fullfilling it")
 	res.writeHead(200)
 	res.write('Hello World!')
 	res.end()
-
-
+	console.log("All good")
 }).listen(process.env.PORT || 80);
